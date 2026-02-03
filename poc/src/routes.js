9237a47a35a051f5c500d13e3fe168a595dd3c4a
Ondrej Valenta <ondrej.valenta@keenmate.com>
2026-02-03 11:46:46 +0100

    - removal of unused code

--- poc/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Main from './Main.svelte';
-import Test from './Test.svelte';
-import Form from './Form.svelte';
-
-export default{
-    '/': Main,
-    '/test': Test, // /#/test
-    '/form': Form
-};
